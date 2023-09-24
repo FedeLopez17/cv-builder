eae82cd020953f26cdb6f4e4c8115e71362d03cc
FedeLopez17 <fedelopez1712@gmail.com>
2023-09-23 22:09:17 -0300

    Add download as PDF feature

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "../../styles/Preview.css";
 import LayoutOne from "./LayoutOne";
+import html2pdf from "html2pdf.js";
 
 export default class Preview extends React.Component {
   render() {
     const downloadPDF = () => {
+      const { firstName, lastName } = this.props.personalInfo;
+
+      const options = {
+        margin: 0,
+        filename: `cv-${firstName?.toLowerCase().trim()}-${lastName
+          ?.toLowerCase()
+          .trim()}.pdf`,
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 3 },
+        jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+      };
+
       const preview = document.getElementById("cv-preview");
+
+      html2pdf()
+        .set(options)
+        .from(preview)
+        .save()
+        .catch((err) => console.error(err));
     };
 
     return (
