2dbdcf7a386e5c5d06c1b789dc840c10bf523cf6
FedeLopez17 <fedelopez1712@gmail.com>
2023-12-22 18:21:20 -0300

    Feat - Initialize project using Vite

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
